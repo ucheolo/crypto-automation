650bbfe3c19cd97f687fc6478e86ed9b64d620a3
ucShin <swc9704@gmail.com>
2024-07-09 18:43:38 +0900

    yml setting

--- src/main/java/com/example/crypto_automatioon/CryptoAutomatioonApplication.java
+++ src/main/java/com/example/crypto_automatioon/CryptoAutomatioonApplication.java
@@ -2,12 +2,14 @@ package com.example.crypto_automatioon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CryptoAutomatioonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CryptoAutomatioonApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(CryptoAutomatioonApplication.class, args);
+		SpringApplication.exit(run);
 	}
 
 }
